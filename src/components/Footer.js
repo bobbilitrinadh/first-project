7a0b9430ae836e3711ce2ee1f327b8559b63c5e8
Trinadh Bobbili <trinadhbobbili.work@gmail.com>
2024-09-14 12:16:58 +0530

    new changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,8 +12,6 @@ const Footer = () => {
           <li><Link to="/">Home</Link></li>
             <li><Link to="/about">About</Link></li>
             <li><Link to="/menu">Menu</Link></li>
-            <li><Link to="/reservations">Reservations</Link></li>
-            <li><Link to="/order">Order Online</Link></li>
             <li><Link to="/login">Login</Link></li>
           </ul>
         </div>
